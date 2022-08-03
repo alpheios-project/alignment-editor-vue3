94bede0a20546362e42fa152407ce025d9df973f
Irina <irina060981@yandex.ru>
2022-08-03 20:52:58 +1000

    add context, quick start and video pages

---
+++ src/_pages/quick-text/index.js
@@ -0,0 +1,7 @@
+import OutputStyle from '@/_pages/quick-text/styles/style.scss'
+import AppController from '@/_pages/quick-text/app-controller.js'
+import App from '@/_pages/quick-text/vue/app.vue'
+
+export {
+  OutputStyle, App, AppController
+}
